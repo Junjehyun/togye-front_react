503fd929ddf951262658fa0fec945d7bcc680d9d
jehyun.jun <h94051987@gmail.com>
2025-06-14 20:32:58 +0900

    tailwind setup

--- togyebu_react_app/src/App.js
+++ togyebu_react_app/src/App.js
@@ -5,7 +5,7 @@ import TodaysPlan from './component/TodaysPlan';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,8 +18,15 @@ function App() {
         >
           Learn React
         </a>
-      </header> */}
-      <TodaysPlan />
+      </header>
+      <div className="p-6 bg-gray-100 min-h-screen">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hello, Tailwind!
+      </h1>
+      <button className="mt-4 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">
+        클릭해 보세요
+      </button>
+    </div>
     </div>
   );
 }
